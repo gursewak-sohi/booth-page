658d7adfd6f513bce1a71d3aeefedefb374c0a0e
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2024-04-12 15:39:22 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to setup the toggle functionality
+    function setupToggleForContainerFull(container) {
+        const contentFull = container.querySelector('.content-full');
+        const toggleButton = container.querySelector('.toggle-btn-full');
+
+        // Initially hide the content
+        contentFull.style.display = 'none';
+
+        // Event listener for the toggle button
+        toggleButton.addEventListener('click', function() {
+            // Check the current state and toggle accordingly
+            if (contentFull.style.display === 'none') {
+                // If hidden, show the content
+                contentFull.style.display = '';
+                toggleButton.querySelector('span').textContent = 'Collapse';
+                toggleButton.querySelector('img').classList.remove('-rotate-90');
+                toggleButton.querySelector('img').classList.add('rotate-90');
+            } else {
+                // If shown, hide the content
+                contentFull.style.display = 'none';
+                toggleButton.querySelector('span').textContent = 'More Info';
+                toggleButton.querySelector('img').classList.remove('rotate-90');
+                toggleButton.querySelector('img').classList.add('-rotate-90');
+            }
+        });
+    }
+
+    // Setup toggle functionality for the specified container
+    document.querySelectorAll('.toggle-container-full').forEach(setupToggleForContainerFull);
+});
